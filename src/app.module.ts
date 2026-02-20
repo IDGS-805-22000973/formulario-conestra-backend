4891fa502fc24458999ee3269f23571505571124
cerveraburrola <cerveraburrolaj@gmail.com>
2026-02-19 23:49:35 -0600

    Cambios para produccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,17 +16,34 @@ import { TestEngineModule } from './test-engine/test-engine.module';
 
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: configService.get<boolean>('DB_SYNC'),
-        logging: configService.get<boolean>('DB_LOGGING'),
-      }),
+      useFactory: (configService: ConfigService) => {
+        const entornoProduccion =
+          configService.get<string>('NODE_ENV') === 'production';
+
+        if (entornoProduccion) {
+          return {
+            type: 'postgres',
+            url: configService.get<string>('DATABASE_URL'),
+            autoLoadEntities: true,
+            synchronize: false,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          };
+        }
+
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          logging: true,
+        };
+      },
     }),
 
     AuthModule,
@@ -36,4 +53,4 @@ import { TestEngineModule } from './test-engine/test-engine.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
