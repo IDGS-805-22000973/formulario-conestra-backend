1a4496a45cee3058529d25f99861abf405b31e8e
cerveraburrola <cerveraburrolaj@gmail.com>
2026-03-14 12:08:32 -0600

    Cambios en el main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { UsersService } from './users/users.service';
 
 
 async function bootstrap() {
@@ -10,28 +9,6 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-
-  const usersService = app.get(UsersService);
-
-  // Crear usuario admin si no existe
-  const correoAdmin = 'admin@test.com';
-
-  const usuarioExistente = await usersService.findOneByEmail(correoAdmin);
-
-  if (!usuarioExistente) {
-    await usersService.create({
-      nombre: 'Administrador',
-      correo: correoAdmin,
-      password: '123456',
-      edad: 30,
-      sexo: 'M',
-      role: 'admin',
-    });
-
-    console.log('Usuario admin creado automáticamente');
-  } else {
-    console.log('Usuario admin ya existe');
-  }
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
